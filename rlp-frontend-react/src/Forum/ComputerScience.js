7fd1363fee1dc2b1301d6ad2722d5c113c34eaca
ArranW123 <65815441+ArranW123@users.noreply.github.com>
2021-02-02 15:22:57 +0000

    Creating ComputerScience.js
    For Forum.js (Thread page)

---
+++ rlp-frontend-react/src/Forum/ComputerScience.js
@@ -0,0 +1,17 @@
+import Header from '../Header/Header';
+import './Forum.css';
+import axios from 'axios';
+import React, {Component} from "react";
+
+function ComputerScience(){
+return(
+    <body>
+        <div>
+            <h1>Welcome to Computer Science!</h1>
+            <p>TESTING!</p>
+        </div>
+    </body>
+
+);
+}
+export default ComputerScience;
