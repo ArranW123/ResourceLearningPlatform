c1b303bfb41d4c9c43ac92309fe08259cc8f49a1
Mohammed Fawaz (Student) <1818096@brunel.ac.uk>
2021-02-03 18:28:04 +0000

    Working on About.js

---
+++ rlp-frontend-react/src/About/About.js
@@ -0,0 +1,27 @@
+import './About.css';
+import axios from 'axios';
+import Header from '../Header/Header';
+import about from './about.png';
+
+function About(){
+  return(
+    <div>
+      <Header />
+      <div className="page-container">
+          <div className="About-Us-info">
+          <h1>
+                About us page for user information about the team
+          </h1>
+          <h2>
+                Program was created by a team of second year developers<br></br>studying at Brunel University London<br></br>
+          </h2>
+          </div>
+          <div className="img">
+              <img src={about} alt="about photo">
+              </img>
+          </div>
+          </div>
+  )
+}
+
+export default About;
