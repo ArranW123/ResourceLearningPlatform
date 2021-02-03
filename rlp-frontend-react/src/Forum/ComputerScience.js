7c6d2ed23297e7dae0c9534d8f8e9f3750c63987
ArranW123 <65815441+ArranW123@users.noreply.github.com>
2021-02-03 19:07:56 +0000

    Update ComputerScience.js

--- rlp-frontend-react/src/Forum/ComputerScience.js
+++ rlp-frontend-react/src/Forum/ComputerScience.js
@@ -1,3 +1,4 @@
+import {Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Header from '../Header/Header';
 import './Forum.css';
 import axios from 'axios';
@@ -6,12 +7,28 @@ import React, {Component} from "react";
 function ComputerScience(){
 return(
     <body>
+        <Header />
         <div>
-            <h1>Welcome to Computer Science!</h1>
-            <p>TESTING!</p>
+        <div>
+            &nbsp;
+            &nbsp;
+
+            <h1>Welcome to Computer Science</h1>
+
+            &nbsp;
+            &nbsp;
+        </div>
+            <div>
+            <li>
+            <Link to={"/Thread1"}><h2>Computer Science is hard! HELP ME!!!</h2></Link>
+            <p>Last Updated: ...</p>
+            <p>Comments: ...</p>
+            <h2></h2>
+            </li>
+            </div>
         </div>
     </body>
 
-);
+    );
 }
 export default ComputerScience;
