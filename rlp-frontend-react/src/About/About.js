347f00cb15111b7f96156dbd3410b8d0ff7a2085
Mohammed Fawaz (Student) <1818096@brunel.ac.uk>
2021-02-04 20:25:37 +0000

    added rest of avatars

--- rlp-frontend-react/src/About/About.js
+++ rlp-frontend-react/src/About/About.js
@@ -34,7 +34,7 @@ function About(){
             <div className="container">
               <img src={avatar1} alt="Avatar1" className="avatar-image" width="512" height="512" />
               <div className="overlay">
-                <div className="text">Moe Fawaz</div>
+                <div className="text">Mohammed</div>
               </div>
             </div>
           </div>
@@ -45,6 +45,46 @@ function About(){
               <img src={avatar2} alt="Avatar2" className="avatar-image" width="512" height="512" />
               <div className="overlay">
                 <div className="text">Ahmed</div>
+              </div>
+            </div>
+          </div>
+          <div className="avatar-container">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{__html: "\n.container {\n  position: relative;\n  width: 50%;\n}\n\n.image {\n  display: block;\n  width: 100%;\n  height: auto;\n}\n\n.overlay {\n  position: absolute;\n  top: 0;\n  bottom: 0;\n  left: 0;\n  right: 0;\n  height: 100%;\n  width: 100%;\n  opacity: 0;\n  transition: .5s ease;\n  background-color: #008CBA;\n}\n\n.container:hover .overlay {\n  opacity: 1;\n}\n\n.text {\n  color: white;\n  font-size: 20px;\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  -webkit-transform: translate(-50%, -50%);\n  -ms-transform: translate(-50%, -50%);\n  transform: translate(-50%, -50%);\n  text-align: center;\n}\n" }} />
+            <div className="container">
+              <img src={avatar3} alt="Avatar3" className="avatar-image" width="512" height="512" />
+              <div className="overlay">
+                <div className="text">Arran</div>
+              </div>
+            </div>
+          </div>
+          <div className="avatar-container">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{__html: "\n.container {\n  position: relative;\n  width: 50%;\n}\n\n.image {\n  display: block;\n  width: 100%;\n  height: auto;\n}\n\n.overlay {\n  position: absolute;\n  top: 0;\n  bottom: 0;\n  left: 0;\n  right: 0;\n  height: 100%;\n  width: 100%;\n  opacity: 0;\n  transition: .5s ease;\n  background-color: #008CBA;\n}\n\n.container:hover .overlay {\n  opacity: 1;\n}\n\n.text {\n  color: white;\n  font-size: 20px;\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  -webkit-transform: translate(-50%, -50%);\n  -ms-transform: translate(-50%, -50%);\n  transform: translate(-50%, -50%);\n  text-align: center;\n}\n" }} />
+            <div className="container">
+              <img src={avatar4} alt="Avatar4" className="avatar-image" width="512" height="512" />
+              <div className="overlay">
+                <div className="text">Sagar</div>
+              </div>
+            </div>
+          </div>
+          <div className="avatar-container">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{__html: "\n.container {\n  position: relative;\n  width: 50%;\n}\n\n.image {\n  display: block;\n  width: 100%;\n  height: auto;\n}\n\n.overlay {\n  position: absolute;\n  top: 0;\n  bottom: 0;\n  left: 0;\n  right: 0;\n  height: 100%;\n  width: 100%;\n  opacity: 0;\n  transition: .5s ease;\n  background-color: #008CBA;\n}\n\n.container:hover .overlay {\n  opacity: 1;\n}\n\n.text {\n  color: white;\n  font-size: 20px;\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  -webkit-transform: translate(-50%, -50%);\n  -ms-transform: translate(-50%, -50%);\n  transform: translate(-50%, -50%);\n  text-align: center;\n}\n" }} />
+            <div className="container">
+              <img src={avatar5} alt="Avatar5" className="avatar-image" width="512" height="512" />
+              <div className="overlay">
+                <div className="text">Keyur</div>
+              </div>
+            </div>
+          </div>
+          <div className="avatar-container">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{__html: "\n.container {\n  position: relative;\n  width: 50%;\n}\n\n.image {\n  display: block;\n  width: 100%;\n  height: auto;\n}\n\n.overlay {\n  position: absolute;\n  top: 0;\n  bottom: 0;\n  left: 0;\n  right: 0;\n  height: 100%;\n  width: 100%;\n  opacity: 0;\n  transition: .5s ease;\n  background-color: #008CBA;\n}\n\n.container:hover .overlay {\n  opacity: 1;\n}\n\n.text {\n  color: white;\n  font-size: 20px;\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  -webkit-transform: translate(-50%, -50%);\n  -ms-transform: translate(-50%, -50%);\n  transform: translate(-50%, -50%);\n  text-align: center;\n}\n" }} />
+            <div className="container">
+              <img src={avatar6} alt="Avatar6" className="avatar-image" width="512" height="512" />
+              <div className="overlay">
+                <div className="text">Sekhar</div>
               </div>
             </div>
           </div>
