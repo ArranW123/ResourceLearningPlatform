627b730ea6cb1fe27777a63faa62ea25d61b7481
Mohammed Fawaz (Student) <1818096@brunel.ac.uk>
2021-02-04 20:11:25 +0000

    2 about avatars added

--- rlp-frontend-react/src/About/About.js
+++ rlp-frontend-react/src/About/About.js
@@ -2,6 +2,12 @@ import './About.css';
 import axios from 'axios';
 import Header from '../Header/Header';
 import about from './about.png';
+import avatar1 from './002-man.png';
+import avatar2 from './013-man.png';
+import avatar3 from './026-man.png';
+import avatar4 from './038-man.png';
+import avatar5 from './041-man.png';
+import avatar6 from './049-man.png';
 
 function About(){
   return(
@@ -22,10 +28,29 @@ function About(){
               </img>
           </div>
           </div>
+          <div className="avatar-container">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{__html: "\n.container {\n  position: relative;\n  width: 50%;\n}\n\n.image {\n  display: block;\n  width: 100%;\n  height: auto;\n}\n\n.overlay {\n  position: absolute;\n  top: 0;\n  bottom: 0;\n  left: 0;\n  right: 0;\n  height: 100%;\n  width: 100%;\n  opacity: 0;\n  transition: .5s ease;\n  background-color: #008CBA;\n}\n\n.container:hover .overlay {\n  opacity: 1;\n}\n\n.text {\n  color: white;\n  font-size: 20px;\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  -webkit-transform: translate(-50%, -50%);\n  -ms-transform: translate(-50%, -50%);\n  transform: translate(-50%, -50%);\n  text-align: center;\n}\n" }} />
+            <div className="container">
+              <img src={avatar1} alt="Avatar1" className="avatar-image" width="512" height="512" />
+              <div className="overlay">
+                <div className="text">Moe Fawaz</div>
+              </div>
+            </div>
+          </div>
+          <div className="avatar-container">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{__html: "\n.container {\n  position: relative;\n  width: 50%;\n}\n\n.image {\n  display: block;\n  width: 100%;\n  height: auto;\n}\n\n.overlay {\n  position: absolute;\n  top: 0;\n  bottom: 0;\n  left: 0;\n  right: 0;\n  height: 100%;\n  width: 100%;\n  opacity: 0;\n  transition: .5s ease;\n  background-color: #008CBA;\n}\n\n.container:hover .overlay {\n  opacity: 1;\n}\n\n.text {\n  color: white;\n  font-size: 20px;\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  -webkit-transform: translate(-50%, -50%);\n  -ms-transform: translate(-50%, -50%);\n  transform: translate(-50%, -50%);\n  text-align: center;\n}\n" }} />
+            <div className="container">
+              <img src={avatar2} alt="Avatar2" className="avatar-image" width="512" height="512" />
+              <div className="overlay">
+                <div className="text">Ahmed</div>
+              </div>
+            </div>
+          </div>
           </body>
           </div>
 
   )
 }
-
 export default About;
