5a9b93cf7da9ce479d486565c1b555c5eed31d65
ArranW123 <65815441+ArranW123@users.noreply.github.com>
2021-02-04 18:48:34 +0000

    Updating Thread1.js & Creating Comment.java

--- groupproject/src/main/java/com/example/groupproject/repository/PasswordEncoder.java
+++ groupproject/src/main/java/com/example/groupproject/repository/PasswordEncoder.java
@@ -1,15 +1,15 @@
-package com.example.groupproject.repository;
+// package com.example.groupproject.repository;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class PasswordEncoder {
+// public class PasswordEncoder {
 
-	public static void main(String[] args) {
-		BCryptPasswordEncoder encode = new BCryptPasswordEncoder();
-		String rawPass = "sagar2021";
-		String encodedPass = encode.encode(rawPass);
+// 	public static void main(String[] args) {
+// 		BCryptPasswordEncoder encode = new BCryptPasswordEncoder();
+// 		String rawPass = "sagar2021";
+// 		String encodedPass = encode.encode(rawPass);
 		
-		System.out.println(encodedPass);
-	}
+// 		System.out.println(encodedPass);
+// 	}
 
-}
+// }
--- rlp-frontend-react/src/Forum/Threads/ComputerScience/Thread1.js
+++ rlp-frontend-react/src/Forum/Threads/ComputerScience/Thread1.js
@@ -4,12 +4,46 @@ import './Thread1.css';
 import axios from 'axios';
 import React, {Component} from "react";
 
+
 function Thread1(){
     return(
         <body>
             <Header />
             <h1>Hello!</h1>
+            <div>
+                <p>I am having trouble with Java programming, can someone help me?</p>
+                <form>
+                    <label for="Name">Name</label> 
+                    <input type = "text"></input>
+                    <br></br>
+                    <label for="Comment">Comment</label><br></br>
+                    <input type="text"></input>
+                    <br></br>
+                    <input type="submit" value="Submit"></input>
+                </form>
+                <div class="comments">
+                </div>
+            </div>
         </body>
     );
 }
+
+function addComment(comment){
+  
+        var commentHtml = `
+        <div class="comment">
+            <div class="top-comment">
+                <p class="user">${comment.author}</p>
+            </div>
+            <p class="comment-ts">${new Date (comment.date).toLocaleString()}</p>
+            <div class="comment-content">
+                ${comment.content}
+            </div>
+        </div>
+        `
+  
+    
+}
+var comments = document.querySelector('.comments');
+
 export default Thread1;
