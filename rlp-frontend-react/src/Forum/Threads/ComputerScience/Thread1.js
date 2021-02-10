2598d28ecca9c70e70ce24c2617ae9ed4cd2cb68
sekhar793 <sekharvarma793@gmail.com>
2021-02-10 20:12:52 +0500

    Fixed css issue

--- rlp-frontend-react/src/Forum/Threads/ComputerScience/Thread1.js
+++ rlp-frontend-react/src/Forum/Threads/ComputerScience/Thread1.js
@@ -1,49 +1,47 @@
-import {Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import Header from '../../../Header/Header';
-import './Thread1.css';
-import axios from 'axios';
-import React, {Component} from "react";
+import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Header from "../../../Header/Header";
+import "./Thread1.css";
+import axios from "axios";
+import React, { Component } from "react";
 
-
-function Thread1(){
-    return(
-        <body>
-            <Header />
-            <h1>Hello!</h1>
-            <div>
-                <p>I am having trouble with Java programming, can someone help me?</p>
-                <form>
-                    <label for="Name">Name</label> 
-                    <input type = "text"></input>
-                    <br></br>
-                    <label for="Comment">Comment</label><br></br>
-                    <input type="text"></input>
-                    <br></br>
-                    <input type="submit" value="Submit"></input>
-                </form>
-                <div class="comments">
-                </div>
-            </div>
-        </body>
-    );
+function Thread1() {
+  return (
+    <body>
+      <Header />
+      <div>
+        <h1>Hello!</h1>
+        <div>
+          <p>I am having trouble with Java programming, can someone help me?</p>
+          <form>
+            <label for="Name">Name</label>
+            <input type="text"></input>
+            <br></br>
+            <label for="Comment">Comment</label>
+            <br></br>
+            <input type="text"></input>
+            <br></br>
+            <input type="submit" value="Submit"></input>
+          </form>
+          <div class="comments"></div>
+        </div>
+      </div>
+    </body>
+  );
 }
 
-function addComment(comment){
-  
-        var commentHtml = `
+function addComment(comment) {
+  var commentHtml = `
         <div class="comment">
             <div class="top-comment">
                 <p class="user">${comment.author}</p>
             </div>
-            <p class="comment-ts">${new Date (comment.date).toLocaleString()}</p>
+            <p class="comment-ts">${new Date(comment.date).toLocaleString()}</p>
             <div class="comment-content">
                 ${comment.content}
             </div>
         </div>
-        `
-  
-    
+        `;
 }
-var comments = document.querySelector('.comments');
+var comments = document.querySelector(".comments");
 
 export default Thread1;
