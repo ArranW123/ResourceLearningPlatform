451748563fc2aa689d9e9deb8753752885edd345
sagarchhetri99 <73656587+sagarchhetri99@users.noreply.github.com>
2021-02-23 18:43:04 +0000

    Values can be inputted in the To-Do box
    -added a button to add value
    -type text
    -allow text to be stored in an arraylist
    -text is displayed
    -added an error function

--- rlp-frontend-react/src/To-do-List/todo.js
+++ rlp-frontend-react/src/To-do-List/todo.js
@@ -2,15 +2,35 @@ import "./todo.css";
 import React from "react";
 
 
-function ToDoList(){
+const ToDoList = () => {
+    const [todos, setTodos] = React.useState([]);
+    const [todo, setTodo] = React.useState("");
 
-
-    return(
-        <body>
-            <h1>To-do List</h1>
-        </body>
-    
-
-    )
-}
-export default ToDoList;
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+  
+      const newTodo = {
+        id: new Date().getTime(),
+        text: todo,
+        completed: false,
+      };
+      setTodos([...todos].concat(newTodo));
+      setTodo("");
+    }
+  
+    return (
+        <div className="ToDoList">
+            <form onSubmit = {handleSubmit}>
+                <input type="text" onChange = {(e) => setTodo(e.target.value)} value = {todo}/>
+                <button type ="submit"> Add To-Do </button>
+            </form>
+            {todos.map((todo) => <div key = {todo.id}>
+                <div>{todo.text}</div>
+                </div>)}
+        </div>
+      
+    );
+  }
+  
+  export default ToDoList;
