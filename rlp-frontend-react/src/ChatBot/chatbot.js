2667d93028857900d38af93c30dbc767a24bb36b
Mohammed Fawaz (Student) <1818096@brunel.ac.uk>
2021-03-02 23:46:10 +0000

    Working on chatroom

--- rlp-frontend-react/src/ChatBot/chatbot.js
+++ rlp-frontend-react/src/ChatBot/chatbot.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './chatbot.css';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/analytics'
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -25,8 +26,27 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
-const [user] = useAuthState(auth);
+function App()
+{
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+    <header>
+      <h1>ChatRoom</h1>
+      <SignOut />
+    </header>
+
+    <section>
+      {user ? <ChatRoom /> : <SignIn />}
+    </section>
+
+    </div>
+  )
+}
+
 
 function SignIn(){
   const signInWithGoogle = () => {
@@ -35,46 +55,68 @@ function SignIn(){
   }
 
   return(
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <>
+    <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
   )
 }
 
 function SignOut(){
   return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 
-function ChatMessage(props) {
-  const {text, uid} = props.message;
-
-  return <p>{text}</p>
-}
-
 function ChatRoom(){
+const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, {idField: 'id'});
 
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.FieldView.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behaviour: 'smooth' });
+  }
+
   return(
     <>
-    <div>
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-    </div>
+    <main>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      <span ref={dummy}></span>
+    </main>
+    <form onSubmit={sendMessage}>
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Type message here" />
+      <button type="submit" disabled={!formValue}></button>
+    </form>
+    </>
   )
 
 }
 
-return (
-  <div>
-  <Header/>
+function ChatMessage(props) {
+  const {text, uid} = props.message;
 
-    <section>
-      {user ? <ChatRoom /> : <SignIn />}
-    </section>
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  </div>
-)
 
-export default chatbot;
+  return(
+    <div className={'message ${messageClass}'}>
+      <p>{text}</p>
+    </div>
+    </>
+  )
+}
+
+export default App;
