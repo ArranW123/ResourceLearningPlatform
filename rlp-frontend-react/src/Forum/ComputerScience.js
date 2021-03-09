3924b0fd5d19442877044cd18623336e37b8c815
AAhmed <73727185+AAhmed202@users.noreply.github.com>
2021-03-09 12:05:57 +0000

    Forum Revert Fix

--- rlp-frontend-react/src/Forum/ComputerScience.js
+++ rlp-frontend-react/src/Forum/ComputerScience.js
@@ -13,23 +13,16 @@ return(
             &nbsp;
             &nbsp;
 
-
             <h1 className='forumTitle'>Welcome to Computer Science</h1>
 
-
             &nbsp;
             &nbsp;
-            <h3>
-            <Link to={'/Thread1'} className="kpbt"> HELP ME! </Link>
-            </h3>
         </div>
-
             <div className = 'bottom'>
             <Link to={"/Thread1"}><h2>Computer Science is hard! HELP ME!!!</h2></Link>
             <p>Last Updated: ...</p>
             <p>Comments: ...</p>
             <h2></h2>
-
             </div>
         </div>
     </body>
